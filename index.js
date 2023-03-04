ef5f6b30fa9569ae59ba153b9fbd9a69fed9c03d
liamgabriel2021 <arbey221@gmail.com>
2023-03-04 00:37:11 -0500

    push Heiner Arevalo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ However, if you have any questions, please send us an email
 to support@webcat.app with the subject "Jr Fullstack Test Questions"
 */
 
-import $t from './libs/test.js'
+import $t from "./libs/test.js";
 
 /*
 1. Data manipulation:
@@ -33,12 +33,32 @@ import $t from './libs/test.js'
   Hint: Use native array methods as well as
     Lodash(https://lodash.com/docs) modules.
 */
-import _ from 'lodash'
-const source = $t.source(1)
+import _ from "lodash";
+const source = $t.source(1);
 $t.answer(1, async () => {
-  // Your code goes here
-  return 
-})
+  const targetData = {
+    balance: 0,
+    income: 0,
+    expenses: 0,
+    byCategories: {},
+  };
+
+  _.forEach(source, (e) => {
+    const { category, amount, type } = e;
+    if (type === "income") {
+      targetData.income += amount;
+    } else if (type === "expense") {
+      targetData.expenses += amount;
+    }
+    targetData.byCategories[category] =
+      (targetData.byCategories[category] || 0) +
+      (type === "income" ? amount : -amount);
+  });
+
+  targetData.balance = targetData.income - targetData.expenses;
+
+  return targetData;
+});
 
 /*
 2. Asynchronous programming: 
@@ -47,11 +67,10 @@ $t.answer(1, async () => {
   3. Finally, return the list of resulting texts as an array.
     
 */
-const $source = $t.source(2)
+const $source = $t.source(2);
 $t.answer(2, async () => {
-    // Your code goes here:
-    // 1. Get ids: $source.getIds()
-    // 2. Get text for every id: $source.getText(id)
-    // 3. Return array of texts
-    return 
-})
+  const ids = await $source.getIds(); // Step 1: Get ids
+  const textPromises = ids.map((id) => $source.getText(id)); // Step 2: Get text for every id
+  const texts = await Promise.all(textPromises); // Wait for all promises to resolve
+  return texts; // Step 3: Return array of texts
+});
